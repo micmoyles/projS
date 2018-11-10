113eead7a20defe6c9160da4bafb62854c6bfcb6
Michael Moyles <mmoyles@puppet>
2018-11-10 18:39:05 +0000

    add some more checks

--- networkCheck.sh
+++ networkCheck.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # steps, advancing to subsequent step assumes failure
-# 1. Can we ping external address (say google DNS 8.8.8.8)
-# 2. Is there an external pointing route (default)
-# 3. Is host part of the correct subnet? 
+# *. Can we ping external address (say google DNS 8.8.8.8)
+# *. Find the route for internet traffic out of the host
+# *. Is the required interface up
+# *. Does it have an IP address - if not should it get one from dhcp or static?
+# *. Is host part of the correct subnet? 
+# *. Is the gateway IP pingable?
 # Example - host is 192.add.re.ss but gateway IP for defualt route is 172.add.re.ss then fail
-# Is the expected interface up
 # 
 EXT_ADDRESS='8.8.8.8'
 NETSTAT=$( which netstat )
@@ -14,6 +16,7 @@ IP=$( which ip )
 TRACEROUTE=$( which traceroute )
 ROUTE=$( which route )
 IFCONFIG=$( which ifconfig )
+ETHTOOL=$(which ethtool )
 desiredCommands='ifconfig netstat ping traceroute ip route' 
 for c in $desiredCommands
 do
@@ -46,16 +49,65 @@ onSuccess () {
 }
 getDefaultRoute () {
 	commandExists netstat || return 1
-	
 }
-getDefaultGateway () {
+getDefaultInterface () {
 	commandExists netstat || return 1
-	GATEWAY=$( $NETSTAT -r | grep default | awk {'print $2'})
-	# check gateway matches IP regex
-	if ! echo $GATEWAY | egrep -q  "([0-9]{1,3}\.)[0-9]{1,3}"
+	INTERFACE=$( netstat -r | grep default | awk {'print $NF'} ) # interface is the last field
+}
+isIPv4 () {
+	if ! echo $1 | egrep -q  "([0-9]{1,3}\.)[0-9]{1,3}"
 	then
-		echo Cannot determine default Gateway. $GATEWAY is not valid
+		return 1
+	else
+		return 0
 	fi
 }
-pingCheck $EXT_ADDRESS && onSuccess pingCheck || echo 'Ping fail, proceeding with checks'  
-getDefaultGateway
+
+getDefaultGateway () {
+	commandExists netstat || return 1
+	GATEWAYIP=$( $NETSTAT -r | grep default | awk {'print $2'})
+	# check gateway matches IP regex
+	if ! echo $GATEWAYIP | egrep -q  "([0-9]{1,3}\.){3}[0-9]{1,3}"
+	then
+		echo Cannot determine default Gateway. $GATEWAYIP is not valid
+		GATEWAYIP=''
+	fi
+}
+getDefaultConfigs () {
+	getDefaultInterface
+	getDefaultGateway
+	echo Gateway IP $GATEWAYIP
+	echo Interface $INTERFACE
+	return 0
+}
+getInterfaceIP () {
+	commandExists ifconfig || return 1
+	INTERFACEIP=$( ifconfig $INTERFACE | grep 'inet addr' | awk {'print $2'} | cut -d: -f2 )
+	isIPv4 $INTERFACEIP || return 1
+	return 0
+}
+#pingCheck $EXT_ADDRESS && onSuccess pingCheck || echo 'Ping fail, proceeding with checks'  
+
+getDefaultConfigs
+# is the interface up
+echo Checking the interface $INTERFACE is up ... 
+if ! $IFCONFIG $INTERFACE &> /dev/null 
+then
+	echo The default interface $INTERFACE is not up, this is a possible problem to investigate
+else
+	echo Default interface is up
+fi
+echo 'Checking the interface has an IP ...'
+if getInterfaceIP $INTERFACE
+then
+	echo Interface $INTERFACE has IP $INTERFACEIP
+else
+	echo Interface $INTERFACE has no IP
+fi
+echo Checking if we can reach the gateway IP $GATEWAYIP ...
+if ! pingCheck $GATEWAYIP
+then
+	echo Cannot ping Gateway IP $GATEWAYIP
+else
+	echo We can
+fi
