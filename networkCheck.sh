4c7f18e474c92740280e6093df48366bb9559299
Michael Moyles <mmoyles@puppet>
2018-11-14 16:13:50 +0000

    need to check it exists and then source or exit

--- networkCheck.sh
+++ networkCheck.sh
@@ -9,6 +9,12 @@
 # *. Is the gateway IP pingable?
 # Example - host is 192.add.re.ss but gateway IP for defualt route is 172.add.re.ss then fail
 # 
+if -f networkCheckFunctions.sh
+then
+	source networkCheckFunctions.sh
+else
+	echo Error - no networkCheckFunctions file found
+fi
 EXT_ADDRESS='8.8.8.8'
 NETSTAT=$( which netstat )
 PING=$( which ping )
@@ -30,83 +36,6 @@ do
 	fi
 done
 
-commandExists () {
-	COM=$( which $1 )
-	if [ $? -ne 0 ]
-	then
-		echo No command $1 found
-		return 1
-	else
-		return 0
-	fi
-}
-
-pingCheck () {
-	commandExists ping || return 1
-	$PING -q -c 5 -W 2 $1 1> $LOG || return 1
-	return 0	
-}
-onSuccess () {
-	echo $1  succeeded, exiting
-	exit 0
-}
-getDefaultRoute () {
-	commandExists netstat || return 1
-}
-getDefaultInterface () {
-	commandExists netstat || return 1
-	INTERFACE=$( netstat -r | grep default | awk {'print $NF'} ) # interface is the last field
-}
-isIPv4 () {
-	if ! echo $1 | egrep -q  "([0-9]{1,3}\.)[0-9]{1,3}"
-	then
-		return 1
-	else
-		return 0
-	fi
-}
-
-getDefaultGateway () {
-	commandExists netstat || return 1
-	GATEWAYIP=$( $NETSTAT -r | grep default | awk {'print $2'})
-	# check gateway matches IP regex
-	if ! echo $GATEWAYIP | egrep -q  "([0-9]{1,3}\.){3}[0-9]{1,3}"
-	then
-		echo Cannot determine default Gateway. $GATEWAYIP is not valid
-		GATEWAYIP=''
-	fi
-}
-getDefaultConfigs () {
-	getDefaultInterface
-	getDefaultGateway
-	getDefaultLog
-	echo Gateway IP $GATEWAYIP
-	echo Interface $INTERFACE
-	echo System log is $SYSTEMLOG
-	return 0
-}
-getInterfaceIP () {
-	commandExists ifconfig || return 1
-	INTERFACEIP=$( ifconfig $1 | grep 'inet addr' | awk {'print $2'} | cut -d: -f2 )
-	isIPv4 $INTERFACEIP || return 1
-	getInterfaceSubnetMask $1
-	return 0
-}
-getInterfaceSubnetMask () {
-	commandExists ifconfig || return 1
-	SUBNETMASK=$( ifconfig $1 | grep 'inet addr' | awk {'print $4'} | cut -d: -f2 )
-	isIPv4 $SUBNETMASK || return 1
-	return 0
-
-}
-getDefaultLog () {
-	for file in /var/log/syslog /var/log/messages
-	do
-		SYSTEMLOG=$file
-		test -f $SYSTEMLOG && return 0
-	done
-	return 1
-}
 if pingCheck $EXT_ADDRESS
 then 
 	echo pingCheck to external address $EXT_ADDRESS succeeded...exiting
